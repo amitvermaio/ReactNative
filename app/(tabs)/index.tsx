7e29751bf00ca023202f39a8de821ba346bd6c11
amitvermaio <amitvns159@gmail.com>
2026-08-18 23:41:55 +0530

    feat: project setup, nativewind, routing & tav navigations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
 import "@/global.css";
 import { Text, View } from "react-native";
 import { Link } from 'expo-router';
- 
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Radhe Radhe
       </Text>
       <Link href={"/onboarding"} className='mt-4 p-5 rounded-xl bg-primary text-white tracking-tight'>
         Go To Onboarding
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
